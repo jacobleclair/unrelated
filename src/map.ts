ea9a29648a7a34af632a069a09677365ee56cce9
jacob <leclairjacob4@gmail.com>
2024-12-11 09:32:52 -0500

    transparency

--- src/map.ts
+++ src/map.ts
@@ -7,6 +7,8 @@ const roomMappings : HTMLImageElement[] = [ // maps room to background; index of
 let playerX = 0; // x position of player
 let playerY = 0; // y posiion of player
 
+const speed = 10;   
+
 enum Directions { up, down, left, right };
 let facing = Directions.up; // direction that the player is facing
 
@@ -15,9 +17,9 @@ function drawMap() {
 
     ctx.drawImage(roomMappings[room], 0 - playerX, 0 - playerY); // draw background
 
-    drawSprite(0, 3, 3, 20, 30, 320, 240, 1.5); // draw sprite
+    drawSprite(0, 3, 3, 20, 30, 320, 240, 2); // draw sprite
 
     // if you've never seen this, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Unary_plus
-    playerX += +directions[3] - +directions[2];
-    playerY += +directions[1] - +directions[0];
+    playerX += (+directions[3] - +directions[2]) * speed;
+    playerY += (+directions[1] - +directions[0]) * speed;
 }
